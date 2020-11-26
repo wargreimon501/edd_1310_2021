bca90bc86825a1a7175cd71752fe3e7cebeb3736
Wargreimon501 <imperialdramon_01@hotmail.com>
2020-11-26 13:00:35 -0600

    actividad

---
+++ ADTS/p_pila.py
@@ -0,0 +1,11 @@
+from pilas import Stack
+pila=Stack()
+pila.push('a')
+pila.push('x')
+pila.to_string()
+pila.push('b')
+pila.push('y')
+pila.to_string()
+var = pila.pop()
+pila.to_string()
+print(f"var ={var}")
