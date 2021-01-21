c41cfb73b5472f555f44e4f81e4e5b6aa25a1e13
Wargreimon501 <imperialdramon_01@hotmail.com>
2021-01-21 12:43:27 -0600

    A 21/1/21

--- ADTS/arboles/arboles_binarios.py
+++ ADTS/arboles/arboles_binarios.py
@@ -33,13 +33,49 @@ class BinarySearchTree:
             self.__recorrido_in(nodo.left)
             print(nodo.data , end =' , ')
             self.__recorrido_in(nodo.right)
+    def __recorrido_pos(self,nodo):
+        if nodo:
+            self.__recorrido_pos(nodo.left)
+            self.__recorrido_pos(nodo.right)
+            print(nodo.data , end = " , ")
+    def __recorrido_pre(self,nodo):
+        if nodo:
+            print(nodo.data , end = " , ")
+            self.__recorrido_pos(nodo.left)
+            self.__recorrido_pos(nodo.right)
     def trasversal(self,format="inorden"):
         if format=="inorden":
             self.__recorrido_in(self.__root)
         elif format == "preorden":
             print("recorrido en pre")
+            self.__recorrido_pre(self.__root)
         elif format =="posorden":
             print("posorden")
+            self.__recorrido_pos(self.__root)
         else:
             print("error , ese formato no existe ")
         print("")
+    def search(self, value):
+        if self.__root == None:
+            return None
+        else:
+            return self.__search(self.__root, value)
+
+    def __search(self, nodo , value):
+        if nodo == None: # vacio?
+            return None
+        elif nodo.data == value:
+            print("Encontrado")
+            return nodo
+        elif value< nodo.data:
+
+            return self.__search(nodo.left, value)
+        else:
+            return self.__search(nodo.right,value)
+    def remove (self,value):
+        encontrado=self.search(value)
+        if encontrado.left==None and encontrado.right:
+            emcontradp=None
+        elif (encontrado != None and encontrado.right== None)  or \
+             (encontrado == None and encontrado.right != None):
+             print("a eliminar ",encontrado.data)
